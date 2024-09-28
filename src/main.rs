dde280827e4b464a5e1d283807b9583a9b9eaee8
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2024-09-28 23:23:43 +0100

    i

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::str::FromStr;
+
+use clap::Parser;
+use sentry::types::Dsn;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    #[clap(short, long, env)]
+    occurrences_service_url: String,
+    #[clap(short, long, env)]
+    sentry_dsn: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+    let Args {
+        occurrences_service_url,
+        sentry_dsn,
+    } = Args::parse();
+    let _guard = sentry::init(sentry::ClientOptions {
+        traces_sample_rate: 1.0,
+        dsn: Some(Dsn::from_str(&sentry_dsn)?),
+        ..Default::default()
+    });
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(sentry_tracing::layer())
+        .try_init()?;
+    Ok(())
 }
