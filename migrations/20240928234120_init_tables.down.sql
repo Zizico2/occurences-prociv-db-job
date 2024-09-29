78f492218e962ad8c37ae374c4789490cf056419
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2024-09-29 02:17:36 +0100

    init mig

--- migrations/20240928234120_init_tables.down.sql
+++ migrations/20240928234120_init_tables.down.sql
@@ -1,3 +1,6 @@
 DROP TABLE occurrences;
+DROP TABLE lu_csrepc;
+DROP TABLE lu_crepc;
 DROP TABLE lu_occurrence_status;
 DROP TABLE lu_grouped_status;
+DROP TABLE lu_occurrence_kind;
--- migrations/20240928234120_init_tables.up.sql
+++ migrations/20240928234120_init_tables.up.sql
@@ -23,13 +23,42 @@ CREATE TABLE
     );
 
 CREATE TABLE
+    lu_crepc (
+        "id" integer PRIMARY KEY,
+        "value" varchar NOT NULL,
+        "display_en" varchar NOT NULL,
+        "display_pt" varchar NOT NULL
+    );
+
+CREATE TABLE
+    lu_csrepc (
+        "id" integer PRIMARY KEY,
+        "value" varchar NOT NULL,
+        "display_en" varchar NOT NULL,
+        "display_pt" varchar NOT NULL
+    );
+
+CREATE TABLE
+    lu_occurrence_kind (
+        "id" integer PRIMARY KEY,
+        "value" varchar NOT NULL,
+        "display_en" varchar NOT NULL,
+        "display_pt" varchar NOT NULL
+    );
+
+CREATE TABLE
     occurrences (
         "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        "kind" integer NOT NULL REFERENCES lu_occurrence_kind ("id"),
         "grouped_status" integer NOT NULL REFERENCES lu_grouped_status ("id"),
         "occurrence_status" integer NOT NULL REFERENCES lu_occurrence_status ("id"),
+        "crepc" integer NOT NULL REFERENCES lu_crepc ("id"),
+        "csrepc" integer NOT NULL REFERENCES lu_csrepc ("id"),
         "anepc_id" varchar NOT NULL,
         "number_of_operatives" integer NOT NULL,
         "number_of_land_means" integer NOT NULL,
         "number_of_water_means" integer NOT NULL,
-        "number_of_air_means" integer NOT NULL
+        "number_of_air_means" integer NOT NULL,
+        "start_date" timestamptz NOT NULL,
+        "data_generated_at" timestamptz NOT NULL
     );
