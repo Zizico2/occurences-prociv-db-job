7b7a9631a3190e89783f5e6480c5ae2f5800fe19
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2024-09-29 03:31:27 +0100

    lu_crepc

--- migrations/20240928234120_init_tables.up.sql
+++ migrations/20240928234120_init_tables.up.sql
@@ -1,49 +1,55 @@
 CREATE TABLE
     lu_grouped_status (
         "id" integer PRIMARY KEY,
-        "value" varchar NOT NULL,
-        "display_en" varchar NOT NULL,
-        "display_pt" varchar NOT NULL
+        "en" varchar NOT NULL,
+        "pt" varchar NOT NULL
     );
 
 INSERT INTO
-    lu_grouped_status ("id", "value", "display_en", "display_pt")
+    lu_grouped_status ("id", "en", "pt")
 VALUES
-    (1, 'dispatching', 'dispatching', 'em despacho'),
-    (2, 'active', 'active', 'em curso'),
-    (3, 'resolving', 'resolving', 'em resolução'),
-    (4, 'concluding', 'concluding', 'em conclusão');
+    (1, 'dispatching', 'em despacho'),
+    (2, 'active', 'em curso'),
+    (3, 'resolving', 'em resolução'),
+    (4, 'concluding', 'em conclusão');
 
 CREATE TABLE
     lu_occurrence_status (
         "id" integer PRIMARY KEY,
         "value" varchar NOT NULL,
-        "display_en" varchar NOT NULL,
-        "display_pt" varchar NOT NULL
+        "en" varchar NOT NULL,
+        "pt" varchar NOT NULL
     );
 
 CREATE TABLE
     lu_crepc (
         "id" integer PRIMARY KEY,
-        "value" varchar NOT NULL,
-        "display_en" varchar NOT NULL,
-        "display_pt" varchar NOT NULL
+        "pt" varchar NOT NULL
     );
+
+INSERT INTO
+    lu_crepc ("id", "pt")
+VALUES
+    (1, 'Norte'),
+    (2, 'Centro'),
+    (3, 'Lisboa e Vale do Tejo'),
+    (4, 'Algarve'),
+    (5, 'Alentejo');
 
 CREATE TABLE
     lu_csrepc (
         "id" integer PRIMARY KEY,
         "value" varchar NOT NULL,
-        "display_en" varchar NOT NULL,
-        "display_pt" varchar NOT NULL
+        "en" varchar NOT NULL,
+        "pt" varchar NOT NULL
     );
 
 CREATE TABLE
     lu_occurrence_kind (
         "id" integer PRIMARY KEY,
         "value" varchar NOT NULL,
-        "display_en" varchar NOT NULL,
-        "display_pt" varchar NOT NULL
+        "en" varchar NOT NULL,
+        "pt" varchar NOT NULL
     );
 
 CREATE TABLE
